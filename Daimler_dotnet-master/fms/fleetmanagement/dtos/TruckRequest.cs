177160febec79090cb79636ddf51fbf974ccf562
Ravis2612 <ravi.rs.s@daimlertruck.com>
2025-01-27 17:36:16 +0530

    Delete Daimler_dotnet-master directory

--- Daimler_dotnet-master/fms/fleetmanagement/dtos/TruckRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace fleetmanagement.dtos
-{
-    public class TruckRequest
-    {
-        public string? Name { get; set; }
-        public string? Model { get; set; }
-        public int Year { get; set; }
-    }
-}
